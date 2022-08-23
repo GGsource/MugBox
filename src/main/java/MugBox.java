1c72606f5fe39793cfd6ad6ee7115815d2ed5c4c
ggonza55 <ggonza55@uic.edu>
2022-08-23 12:13:58 -0500

    Can now pick directory and store string

--- src/main/java/MugBox.java
+++ src/main/java/MugBox.java
@@ -1,13 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 public class MugBox extends Application{
     private Button b1;
-    private TextField t1;
+    DirectoryChooser dirChooser = new DirectoryChooser();
     
     public static void main(String[] args) {
         launch(args);
@@ -15,12 +16,19 @@ public class MugBox extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        b1 = new Button("Button #1");
-        t1 = new TextField();
+        b1 = new Button("Select a folder");
+        Label chosenPathLabel = new Label();
 
-        Scene myScene = new Scene(new VBox(20, b1, t1));
+        b1.setOnAction(click->{
+            //On click this button will give the user a prompt to select a directory
+            dirChooser.setTitle("Select Directory");
+            String selectedDirPath = dirChooser.showDialog(primaryStage).getAbsolutePath();
+            chosenPathLabel.setText(selectedDirPath);
+        });
+
+        Scene myScene = new Scene(new VBox(20, b1, chosenPathLabel), 400, 400);
         primaryStage.setScene(myScene);
-        primaryStage.setTitle("Welcome to JavaFX");
+        primaryStage.setTitle("Project Mugbox");
         primaryStage.show();
 
     }
